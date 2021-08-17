1466ac9cf2f4e74baa3c591276f34ffb384a2a28
Pretender324 <r1kuchicken5761@gmail.com>
2021-08-17 15:10:25 +0900

    finish chapter7

--- MiniMart/Application/Views/ProductDetailPageView.swift
+++ MiniMart/Application/Views/ProductDetailPageView.swift
@@ -9,9 +9,57 @@ import SwiftUI
 
 struct ProductDetailPageView: View {
     var product: FetchProductsQuery.Data.Product
+    @EnvironmentObject var cartState: CartState
+    @State var isCartViewPresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle(product.name)
+        VStack(alignment: .leading) {
+            ScrollView {
+                RemoteImage(urlString: product.imageUrl)
+                VStack(alignment: .leading) {
+                    Text(product.name)
+                        .font(.title)
+                    Spacer()
+                        .frame(height: 32)
+                    Text("\(product.price)")
+                        .font(.title)
+                        + Text("円")
+                    Spacer()
+                        .frame(height: 32)
+                    Text(product.summary)
+                    Spacer()
+                        .frame(height: 32)
+                    Button(action: {
+                        cartState.products.append(product)
+                    }, label: {
+                        Text("カートに追加")
+                            .padding(.vertical, 10)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+                .padding()
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing){
+                Button(action: {
+                    self.isCartViewPresented = true
+                }) {
+                    VStack {
+                        Image(systemName: "folder")
+                        Text(cartState.products.count.description)
+                        }
+                }
+            }
+        }
+        .sheet(isPresented: $isCartViewPresented){
+            NavigationView{
+                CartPageView()
+            }
+        }
     }
 }
 
--- MiniMart/Application/Views/ProductListPageView.swift
+++ MiniMart/Application/Views/ProductListPageView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ProductListPageView: View {
     @State var products: [FetchProductsQuery.Data.Product] = []
     @State var isCartViewPresented: Bool = false
+    @EnvironmentObject var cartState: CartState
     
     var body: some View {
         List(products, id: \.id) { product in
@@ -48,7 +49,10 @@ struct ProductListPageView: View {
                 Button(action: {
                     self.isCartViewPresented = true
                 }) {
-                    Image(systemName: "folder")
+                    VStack{
+                        Image(systemName: "folder")
+                        Text(cartState.products.count.description)
+                        }
                 }
             }
         }
